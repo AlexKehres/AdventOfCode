b8b40773b0bd29190bf845626199e1db48da06de
AlexKehres <75394303+AlexKehres@users.noreply.github.com>
2020-12-03 15:32:23 -0500

    Turn Part2 into function

--- 2020/Day 3/Part2.ps1
+++ 2020/Day 3/Part2.ps1
@@ -1,61 +1,44 @@
+function FindTrees {
+
+    param( 
+        [Parameter(Mandatory)][Array]$TreeScape,
+        [Parameter(Mandatory)][int]$Down,
+        [Parameter(Mandatory)][int]$Right
+    )
+
+    # Iterate through each row, moving down/right a set number of rows/values.
+    # If the spot we land on contains a tree (#), TreeCount += 1
+    # Do this until we've reached the end of the TreeScape
+
+    $TreeCount = 0
+    $t = 0
+    for ($i=0; $i -lt $TreeScape.Count; $i += $Down) {
+        if ($TreeScape[$i][$t] -eq "#") {$TreeCount++}
+        $t += $Right
+    }
+
+    return $TreeCount
+
+}
+
+## MAIN ##
+
 $TreeScape_Orig = Get-Content -Path ".\input.txt"
 
 # Expand out TreeScape. We will never need more than the num rows
 
 $TreeScape_Alt = $TreeScape_Orig | foreach {$_ * $TreeScape_Orig.Count}
 
-# Iterate through each row, moving down/right a set number of rows/values.
-# If the spot we land on contains a tree (#), TreeCount += 1
-# Do this until we've reached the end of the TreeScape
-# Repeat for the 5 movement scenarios.
+# Determine number of trees hit for the 5 passes
 
-# Down 1, Right 1
-
-$TreeCount_1 = 0
-$t = 0
-for ($i=0; $i -lt $TreeScape_Alt.Count; $i++) {
-    if ($TreeScape_Alt[$i][$t] -eq "#") {$TreeCount_1++}
-    $t += 1
-}
-
-# Down 1, Right 3 (Solution from Part 1)
-
-$TreeCount_2 = 0
-$t = 0
-for ($i=0; $i -lt $TreeScape_Alt.Count; $i++) {
-    if ($TreeScape_Alt[$i][$t] -eq "#") {$TreeCount_2++}
-    $t += 3
-}
-
-# Down 1, Right 5
-
-$TreeCount_3 = 0
-$t = 0
-for ($i=0; $i -lt $TreeScape_Alt.Count; $i++) {
-    if ($TreeScape_Alt[$i][$t] -eq "#") {$TreeCount_3++}
-    $t += 5
-}
-
-# Down 1, Right 7
-
-$TreeCount_4 = 0
-$t = 0
-for ($i=0; $i -lt $TreeScape_Alt.Count; $i++) {
-    if ($TreeScape_Alt[$i][$t] -eq "#") {$TreeCount_4++}
-    $t += 7
-}
-
-# Down 2, Right 1
-
-$TreeCount_5 = 0
-$t = 0
-for ($i=0; $i -lt $TreeScape_Alt.Count; $i += 2) {
-    if ($TreeScape_Alt[$i][$t] -eq "#") {$TreeCount_5++}
-    $t++
-}
+$Pass1 = FindTrees -TreeScape $TreeScape_Alt -Down 1 -Right 1
+$Pass2 = FindTrees -TreeScape $TreeScape_Alt -Down 1 -Right 3
+$Pass3 = FindTrees -TreeScape $TreeScape_Alt -Down 1 -Right 5
+$Pass4 = FindTrees -TreeScape $TreeScape_Alt -Down 1 -Right 7
+$Pass5 = FindTrees -TreeScape $TreeScape_Alt -Down 2 -Right 1
 
 # Once we have the number of trees for each sled pass, multiply them all together and return
 
-$Mult = $TreeCount_1 * $TreeCount_2 * $TreeCount_3 * $TreeCount_4 * $TreeCount_5
+$Mult = $Pass1 * $Pass2 * $Pass3 * $Pass4 * $Pass5
 
 return $Mult
